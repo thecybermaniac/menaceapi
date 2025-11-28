232ed7e56176c6314cdbe8db8aeff87ee84de815
Harrison | The Cybermaniac <thetruebeastist@gmail.com>
2025-11-28 16:07:28 +0100

    Added Vercel Insights package

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    <SpeedInsights />
   </QueryClientProvider>
 );
 
